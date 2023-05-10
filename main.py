9756a148120b0b301777319354af440ed91315e2
Eli Policape <policapee@gmail.com>
2023-05-10 00:25:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "Elishah J Policape Resume.pdf"
+resume_file = current_dir / "assets" / "Elishah J. Policape Resume.pdf"
 profile_pic = current_dir / "assets" / "eli_pro_pic.jpg"
 
 
